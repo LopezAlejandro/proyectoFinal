f39d8bb39d05e42f5173906c776ed26483ab014e
Alejandro <lopalejandro@gmail.com>
2023-07-03 18:59:43 -0300

    Corregido el acceso a la lista completa

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -51,7 +51,7 @@ redirect: 'follow'
 fetch(this.url, options)
 .then(function () {
 alert("Registro modificado")
-window.location.href = "../templates/productos.html";
+window.location.href = "../templates/buscar.html";
 })
 .catch(err => {
 console.error(err);
