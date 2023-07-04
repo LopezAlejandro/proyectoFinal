4aeb2ef17366b64cca6ad651389c3a98b1e625f8
Alejandro Lopez <lopalejandro@gmail.com>
2023-07-04 15:02:10 -0300

    mejora de la interfaz. agregue la imagen

--- js/buscar.js
+++ js/buscar.js
@@ -6,29 +6,35 @@ createApp({
       //url: "http://127.0.0.1:5000/",
       searchTerm: "",
       searchField: "autor",
+      searchF: false,
       // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
       //url:'https://alopez.pythonanywhere.com/', // si ya lo subieron a pythonanywhere
     };
   },
   methods: {
     async search() {
-      
       if (this.searchTerm == "") {
         // Si la búsqueda está vacía, se muestra un mensaje de error o se realiza otra acción apropiada.
         console.log("Por favor, ingresa un término de búsqueda.");
         return;
-      }    
-      
-          if (this.searchField == "autor") {
+      }
+
+      if (this.searchField == "autor") {
         this.url = "https://alopez.pythonanywhere.com/sumario_a/";
       } else {
         this.url = "https://alopez.pythonanywhere.com/sumario_t/";
       }
-          
+
       try {
         const response = await fetch(this.url + this.searchTerm);
         const data = await response.json();
-        this.sumarios = data;
+        if (data.length > 0) {
+          this.sumarios = data;
+        } else {
+          this.sumarios = [];
+          this.searchF = true;
+        }
+
         //console.log(this.sumarios);
       } catch (error) {
         console.error("Error al buscar:", error);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
-<div class="container">
-<a class="navbar-brand" href="../templates/buscar.html">Biblioteca FADU -UBA</a>
+document.getElementById(
+  "header"
+).innerHTML = `<img src="../img/narquitectura.png" class="img-fluid" alt="Nuestra Arquitectura">
 
-</div>
-</nav>`
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="http://biblioteca.fadu.uba.ar">Inicio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="./buscar.js">Buscar</a>
+        <a class="nav-link disabled" href="../templates/producto_nuevo.html">Alta de Sumarios</a>
+      </div>
+    </div>
+  </div>
+</nav>`;
